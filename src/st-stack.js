32065b9ddc689f0f6047e72255176bd4ba080fc4
vostavhy <vostavhy@gmail.com>
2021-10-14 14:11:15 +0300

    feat: implement stack

--- src/st-stack.js
+++ src/st-stack.js
@@ -12,19 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
- module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+module.exports = class Stack {
+  constructor() {
+    this.array = [];
+  }
+
+  push(element) {
+    this.array.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array[this.array.length - 1];
   }
-}
+};
--- src/test.js
+++ src/test.js
@@ -1,36 +1,25 @@
-function ListNode(x) {
-  this.value = x;
-  this.next = null;
-}
-
-function convertArrayToList(arr) {
-  return arr.reverse().reduce((acc, cur) => {
-    if (acc) {
-      const node = new ListNode(cur);
-      node.next = acc;
-      return node;
-    }
-
-    return new ListNode(cur);
-  }, null);
-}
-
-let result = null;
-
-function removeKFromList(l, k) {
-  if (!result) result = l;
-  if (!l.next) return result;
-  if (l.value === k) {
-    result = l.next;
-    return removeKFromList(l.next, k);
+class Stack {
+  constructor() {
+    this.array = [];
   }
-  if (l.next.value === k) {
-    l.next = l.next.next;
-    return removeKFromList(l, k);
+
+  push(element) {
+    this.array.push(element);
   }
-  return removeKFromList(l.next, k);
+
+  pop() {
+    return this.array.pop();
+  }
+
+  peek() {
+    return this.array[this.array.length - 1];
+  }
 }
 
-let listNodes = convertArrayToList([3, 1, 2, 3, 4, 5]);
-//console.log(listNodes);
-console.log(removeKFromList(listNodes, 3));
+const stack = new Stack();
+
+stack.push(1); // adds the element to the stack
+
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.pop());
