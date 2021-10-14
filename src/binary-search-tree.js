5667bd674c2ce9ed54ba3cdb9002e37b5644fe75
vostavhy <vostavhy@gmail.com>
2021-10-15 00:20:00 +0300

    feat: implement some additional functionality

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -3,44 +3,107 @@ const { NotImplementedError } = require('../extensions/index.js');
 // const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 module.exports = class BinarySearchTree {
+  constructor() {
+    this._root = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let node = new Node(data);
+    if (!this._root) {
+      this._root = node;
+      return;
+    }
+    this.addNode(node, this._root);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if (this.find(data) === null) return false;
+    return true;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return this.findNode(data, this._root);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    if (this.find(data) === null) return;
+    this._root = this.removeNode(data, this._root);
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.getMinData(this._root);
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.getMaxData(this._root);
   }
 
+  addNode(nodeInsert, nodeCheck) {
+    if (nodeInsert.data > nodeCheck.data) {
+      if (nodeCheck.right != null)
+        return this.addNode(nodeInsert, nodeCheck.right);
+      else {
+        nodeCheck.right = nodeInsert;
+        return;
+      }
+    }
+
+    if (nodeInsert.data < nodeCheck.data) {
+      if (nodeCheck.left != null)
+        return this.addNode(nodeInsert, nodeCheck.left);
+      else nodeCheck.left = nodeInsert;
+    }
+  }
+
+  findNode(dataFind, nodeCheck) {
+    if (nodeCheck === null) return null;
+    if (nodeCheck.data === dataFind) return nodeCheck;
+
+    if (dataFind > nodeCheck.data)
+      return this.findNode(dataFind, nodeCheck.right);
+    if (dataFind < nodeCheck.data)
+      return this.findNode(dataFind, nodeCheck.left);
+  }
+
+  getMaxData(checkNode) {
+    if (checkNode.data === null) return null;
+    if (checkNode.right === null) return checkNode.data;
+    return this.getMaxData(checkNode.right);
+  }
+
+  getMinData(checkNode) {
+    if (!checkNode.data) return null;
+    if (!checkNode.left) return checkNode.data;
+    return this.getMinData(checkNode.left);
+  }
+
+  removeNode(dataRemove, nodeCheck) {
+    if (nodeCheck === null) return nodeCheck;
+
+    if (dataRemove === nodeCheck.data) {
+      if (!nodeCheck.left) return nodeCheck.right;
+      if (!nodeCheck.right) return nodeCheck.left;
+      nodeCheck.data = this.min(nodeCheck.right);
+      nodeCheck.right = this.removeNode(nodeCheck.data, nodeCheck.right);
+    }
+    if (dataRemove > nodeCheck.data)
+      nodeCheck.right = this.removeNode(dataRemove, nodeCheck.right);
+    if (dataRemove < nodeCheck.data)
+      nodeCheck.left = this.removeNode(dataRemove, nodeCheck.left);
+    return nodeCheck;
+  }
+};
+
+function Node(data) {
+  this.data = data;
+  this.left = null;
+  this.right = null;
 }
--- src/test.js
+++ src/test.js
@@ -1,42 +1,122 @@
-class Queue {
+class BinarySearchTree {
   constructor() {
-    this.head = null;
-    this.tail = null;
+    this._root = null;
   }
 
-  getUnderlyingList() {
-    return this.tail;
+  root() {
+    return this._root;
   }
 
-  enqueue(value) {
-    let node = new ListNode(value);
+  add(data) {
+    let node = new Node(data);
+    if (!this._root) {
+      this._root = node;
+      return;
+    }
+    this.addNode(node, this._root);
+  }
 
-    if (!this.tail) {
-      this.head = node;
-      this.tail = node;
-    } else {
-      //node.next = this.tail;
-      //this.tail = node;
-      this.head.next = node;
-      this.head = node;
+  has(data) {
+    if (this.find(data) === null) return false;
+    return true;
+  }
+
+  find(data) {
+    return this.findNode(data, this._root)[0];
+  }
+
+  remove(data) {
+    if (this.find(data) === null) return;
+    //let nodeToDelete = this.findNode(data, this._root)[0];
+    //let parentNode = this.findNode(data, this._root)[1];
+
+    //if (!nodeToDelete.left && !nodeToDelete.right) {
+    //  nodeToDelete = null;
+    //  return;
+    //}
+
+    this._root = this.removeNode(data, this._root);
+  }
+
+  min() {
+    return this.getMinData(this._root);
+  }
+
+  max() {
+    return this.getMaxData(this._root);
+  }
+
+  addNode(nodeInsert, nodeCheck) {
+    if (nodeInsert.data > nodeCheck.data) {
+      if (nodeCheck.right != null)
+        return this.addNode(nodeInsert, nodeCheck.right);
+      else {
+        nodeCheck.right = nodeInsert;
+        return;
+      }
+    }
+
+    if (nodeInsert.data < nodeCheck.data) {
+      if (nodeCheck.left != null)
+        return this.addNode(nodeInsert, nodeCheck.left);
+      else nodeCheck.left = nodeInsert;
     }
   }
 
-  dequeue() {
-    let value = this.tail.value;
-    this.tail = this.tail.next;
-    return value;
+  findNode(dataFind, nodeCheck, nodeParent) {
+    if (nodeCheck === null) return [null, null];
+    if (nodeCheck.data === dataFind) return [nodeCheck, nodeParent];
+
+    if (dataFind > nodeCheck.data)
+      return this.findNode(dataFind, nodeCheck.right, nodeCheck);
+    if (dataFind < nodeCheck.data)
+      return this.findNode(dataFind, nodeCheck.left, nodeCheck);
+  }
+
+  getMaxData(checkNode) {
+    if (checkNode.data === null) return null;
+    if (checkNode.right === null) return checkNode.data;
+    return this.getMaxData(checkNode.right);
+  }
+
+  getMinData(checkNode) {
+    if (!checkNode.data) return null;
+    if (!checkNode.left) return checkNode.data;
+    return this.getMinData(checkNode.left);
+  }
+
+  removeNode(dataRemove, nodeCheck) {
+    if (nodeCheck === null) return nodeCheck;
+
+    if (dataRemove === nodeCheck.data) {
+      if (!nodeCheck.left) return nodeCheck.right;
+      if (!nodeCheck.right) return nodeCheck.left;
+      nodeCheck.data = this.min(nodeCheck.right);
+      nodeCheck.right = this.removeNode(nodeCheck.data, nodeCheck.right);
+    }
+    if (dataRemove > nodeCheck.data)
+      nodeCheck.right = this.removeNode(dataRemove, nodeCheck.right);
+    if (dataRemove < nodeCheck.data)
+      nodeCheck.left = this.removeNode(dataRemove, nodeCheck.left);
+    return nodeCheck;
   }
 }
 
-function ListNode(x) {
-  this.value = x;
-  this.next = null;
+function Node(data) {
+  this.data = data;
+  this.left = null;
+  this.right = null;
 }
 
-const queue = new Queue();
-queue.enqueue(5);
-queue.enqueue(6);
-queue.enqueue(7);
-console.log('5', queue.dequeue());
-console.log('6', queue.dequeue());
+const tree = new BinarySearchTree();
+tree.add(9);
+tree.add(14);
+tree.add(54);
+tree.add(2);
+tree.add(6);
+tree.add(8);
+tree.add(31);
+tree.add(1);
+tree.remove(6);
+tree.remove(2);
+console.log(tree.min(), 1);
